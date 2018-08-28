955f798ce157ad4f97bb5ed23c7e3d4b1b568a9c
mkennani <mehdi.kennani@gmail.com>
2018-08-28 10:19:28 +0200

    On remet la methode abstract dans la classe operation

--- poo/src/main/java/com/aubay/poo/calculatrice/Operation.java
+++ poo/src/main/java/com/aubay/poo/calculatrice/Operation.java
@@ -1,7 +1,10 @@
 package com.aubay.poo.calculatrice;
 
+import java.math.BigDecimal;
+
 public abstract class Operation {
 	
 	//on peut faire une division addition soustraction ou multiplication
+	public abstract BigDecimal faireOperation();
 	
 }
